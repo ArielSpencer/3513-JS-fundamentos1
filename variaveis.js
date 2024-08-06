1254e6a0754b8386200526eeb9b03e4fca961c09
Ariel Spencer <arielspencer.tech@gmail.com>
2024-08-06 19:17:43 -0300

    exercícios(variaveis):resolução de 1 a 6

--- variaveis.js
+++ variaveis.js
@@ -13,3 +13,75 @@ console.log(estudante, professora, sala);
 // observações:
 // var é menos utilizado hoje em dia porque pode levar a comportamentos inesperados devido ao seu escopo de função e falta de bloqueio de redeclaração,
 // enquanto let e const oferecem controle mais preciso e previsível sobre o escopo e a mutabilidade das variáveis.
+
+
+// ------------------------------------------------------------------------------------------------------
+// Exercícios:
+
+console.log('exercício 1:');
+
+const text = 'Hello';
+const number = '123';
+const boolean = 'true';
+
+console.log(text);
+console.log(number);
+console.log(boolean);
+
+console.log('exercício 2:');
+
+const firstName = 'ariel';
+const lastName = 'spencer';
+const fullName = `${firstName} ${lastName}`;
+
+console.log(fullName);
+
+console.log('exercício 3:');
+
+const type = 'World';
+const code = '321';
+const worldName = type + ': ' + code;
+
+console.log(worldName);
+
+console.log('exercício 4:');
+
+let music = 'expresso - sabrina carpenter';
+console.log(music);
+music = 'me too - meghan trainor';
+console.log(music);
+
+console.log('exercício 5-A:');
+
+var outside = 'fora';
+
+if (outside === 'fora') {
+  var inside = 'dentro';
+  console.log(inside);
+  console.log(outside);
+}
+
+console.log(inside);
+console.log(outside);
+
+console.log('exercício 5-B:');
+
+let fora = 'outside';
+
+if (fora === 'outside') {
+  let dentro = 'inside';
+  console.log(fora);
+  console.log(dentro);
+}
+
+console.log(fora);
+// console.log(dentro);
+// ReferenceError: dentro is not defined
+
+console.log('exercício 6:');
+let weather = 'sol';
+if (weather === 'chuva') {
+  console.log('levar guarda-chuva');
+} else {
+  console.log('não levar guarda-chuva')
+}
