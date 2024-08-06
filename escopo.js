9f1b976ba61017a7a833fe3efb857d8f1b4726a6
Ariel Spencer <arielspencer.tech@gmail.com>
2024-08-05 22:17:25 -0300

    o que são escopos no js

---
+++ escopo.js
@@ -0,0 +1,14 @@
+let estudante = 'Caroline';
+
+if (1 > 0) {
+  const estudante = 'Ana';
+  console.log(estudante);
+}
+
+console.log(estudante);
+
+// Escopo é a área do código onde uma variável é definida e pode ser acessada.
+
+// Escopo Global: A variável é acessível em qualquer parte do código;
+// Escopo Local: A variável é acessível apenas dentro da função ou bloco onde foi definida;
+// Escopo do Bloco: A variável é acessível apenas dentro do bloco de código delimitado por chaves {} (usado com let e const).
